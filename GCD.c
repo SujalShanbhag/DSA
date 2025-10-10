720ba24a59f8a663b158e952501a91d7cc4da632
Sujal Shanbhag <sujalgshanbhag1303@gmail.com>
2025-10-10 15:02:22 +0530

    Add files via upload

---
+++ GCD.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int GCD(int a,int b){
+if(a==0){
+    return b;
+}
+else{
+return GCD(b%a,a);
+}
+}
+int main()
+{
+    int a,b;
+    printf("\nRead values for a and b:\n");
+    scanf("%d%d",&a,&b);
+    printf("GCD of %d and %d is %d",a,b,GCD(a,b));
+    return 0;
+}
